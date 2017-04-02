d80b03153b059e03e28a57a7fd883362cd112364
Suhvan <soohvan@gmail.com>
2017-04-02 17:27:04 +0500

    stage end

--- Rehab/Assets/src/GameCore.cs
+++ Rehab/Assets/src/GameCore.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class GameCore : MonoBehaviour {
 
+	public Eye Eye;
 	public DialogsCore dialogSystem;
 	public static GameCore instance { private set; get; }
+
+	bool stageCompleted;
 
 	// Use this for initialization
 	void Start () {
 		instance = this;
 		dialogSystem.OnGameStart();
+		stageCompleted = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (stageCompleted)
+			return;
+
+		if(Eye.Completed)
+		{
+			stageCompleted = true;
+			dialogSystem.OnGameEnd();
+		}
 	}
+	
 }
