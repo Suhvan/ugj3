7c9be4c273aa94ebaad62320cabcf82e85389a23
Suhvan <soohvan@gmail.com>
2017-04-04 02:10:37 +0500

    пробел для хвата

--- Rehab/Assets/src/Hand.cs
+++ Rehab/Assets/src/Hand.cs
@@ -2,6 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum HandState
+{
+	Leaning,
+	WaitingGrab,
+	Grabing,
+	WaitingRelease
+}
+
+
 public class Hand : MonoBehaviour {
 
 	[SerializeField]
@@ -31,27 +40,30 @@ public class Hand : MonoBehaviour {
 	[SerializeField]
 	AnimationCurve resistCurve;
 
-	bool m_directingUp;
+	HandState m_state;
 
-	bool directingUp
+	HandState State
 	{
 		get
 		{
-			return m_directingUp;
+			return m_state;
 		}
 		set
 		{
-			m_directingUp = value;
-			if (m_directingUp)
+			m_state = value;
+			switch (m_state)
 			{
-				freeHand.color = Color.white;
-				gripHand.color = Color.clear;
-			}
-			else
-			{
-				freeHand.color = Color.clear;
-				gripHand.color = Color.white;
-			}
+				case HandState.Grabing:
+				case HandState.WaitingRelease:
+					freeHand.color = Color.clear;
+					gripHand.color = Color.white;
+					break;
+				case HandState.Leaning:
+				case HandState.WaitingGrab:				
+					freeHand.color = Color.white;
+					gripHand.color = Color.clear;
+					break;
+			}			
 		}
 	}
 
@@ -64,6 +76,34 @@ public class Hand : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
+		switch (State)
+		{
+			case HandState.WaitingGrab:
+				if (!Input.GetKeyDown(KeyCode.Space))
+					return;				
+				State = HandState.Grabing;
+				break;
+			case HandState.WaitingRelease:
+				if (!Input.GetKeyUp(KeyCode.Space))
+					return;				
+				State = HandState.Leaning;
+				break;
+			case HandState.Grabing:
+				if (!Input.GetKey(KeyCode.Space))
+				{
+					State = HandState.Leaning;
+					return;
+				}
+				break;
+			case HandState.Leaning:
+				if (Input.GetKey(KeyCode.Space))
+				{
+					State = HandState.Grabing;
+					return;
+				}
+				break;
+		}
+
 		float scrolVal = 0;
         if (useScroll)
 			scrolVal = Input.GetAxis("Mouse ScrollWheel");
@@ -74,25 +114,27 @@ public class Hand : MonoBehaviour {
 			return;
 		var distance = transform.position.y - yMin - backGround.transform.position.y;
         scrolVal /= 1 + resist  * resistCurve.Evaluate(distance / MaxDistance);
+
 		Debug.Log(string.Format("Cur resistL {0}", 1 + resist * resistCurve.Evaluate(distance / MaxDistance)));
-		if (directingUp)
+
+		switch (State)
 		{
-			if (scrolVal < 0)
+			case HandState.Leaning:
+				if (scrolVal < 0)
+					return;
+				transform.Translate(Vector3.up * scrolVal);
+
+				if (transform.localPosition.y >= yMax)
+					State = HandState.WaitingGrab;
 				return;
-			transform.Translate(Vector3.up * scrolVal);	
-					
-            if (transform.localPosition.y >= yMax)
-				directingUp = false;
-			return;
-		}
-
-		if (scrolVal > 0)
-			return;
-		transform.Translate(Vector3.up * scrolVal);
-		backGround.transform.Translate(Vector3.up * scrolVal);
-		if (transform.localPosition.y <= yMin)
-			directingUp = true;
-
-
+			case HandState.Grabing:
+				if (scrolVal > 0)
+					return;
+				transform.Translate(Vector3.up * scrolVal);
+				backGround.transform.Translate(Vector3.up * scrolVal);
+				if (transform.localPosition.y <= yMin)
+					State = HandState.WaitingRelease;
+				return;
+		}	
 	}
 }
